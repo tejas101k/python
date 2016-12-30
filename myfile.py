89dd088160bb4a231b4388150cf6a7fc094bb925
Tejas <tejassingh649@rediffmail.com>
2016-12-30 11:26:06 +0000

    if or else

--- myfile.py
+++ myfile.py
@@ -3,3 +3,9 @@ print("hi " + name)
 
 age = input("how old r u ")
 print("You are " + age + " yers old")
+
+answer = input("do u like pizza")
+if answer = "yes"
+    print("unhealthy")
+else:
+    print("same")
