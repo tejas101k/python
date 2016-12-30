882e2a696b9c29c551d91cbd6cf4d069f9e8d64e
Tejas <tejassingh649@rediffmail.com>
2016-12-30 11:27:22 +0000

    improve

--- myfile.py
+++ myfile.py
@@ -5,7 +5,7 @@ age = input("how old r u ")
 print("You are " + age + " yers old")
 
 answer = input("do u like pizza")
-if answer = "yes"
+if answer == "yes"
     print("unhealthy")
 else:
     print("same")
